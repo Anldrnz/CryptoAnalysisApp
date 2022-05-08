e4bcc4b4b92ea6d43b66955b8dcb329b26a2769d
Anldrnz <forgamesoyun@hotmail.com>
2022-05-08 23:45:02 +0300

    host address change

--- dashhh.py
+++ dashhh.py
@@ -689,4 +689,4 @@ app.layout = html.Div(children=[
 if __name__ == '__main__':
     host = socket.gethostbyname(socket.gethostname())
     print(host)
-    app.run_server(debug=True, host=host, port=8090)
+    app.run_server(debug=True, host='0.0.0.0', port=8090)
